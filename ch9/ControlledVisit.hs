6184de1ec6a8ec0e72781adbf31abe991dea6f72
Baylac-Jacqué Félix <baylac.felix@gmail.com>
2017-03-29 16:04:03 +0200

    Implemented controlled visit.

---
+++ ch9/ControlledVisit.hs
@@ -0,0 +1,49 @@
+module ControllerVisit where
+
+import System.IO(openFile, IOMode(..), hClose, hFileSize)
+import System.Directory(Permissions, searchable, getDirectoryContents, getPermissions, getModificationTime)
+import System.FilePath((</>))
+import Control.Monad (forM, liftM)
+import Control.Exception(handle, SomeException(SomeException), bracket)
+import Data.Time(UTCTime)
+
+data Info = Info {
+    infoPath :: FilePath
+  , infoPerms :: Maybe Permissions
+  , infoSize :: Maybe Integer
+  , infoModTime :: Maybe UTCTime
+} deriving (Eq, Ord, Show)
+
+maybeIO :: IO a -> IO (Maybe a)
+maybeIO act = handle (\(SomeException _)-> return Nothing) (Just `liftM` act)
+
+getInfo :: FilePath -> IO Info
+getInfo path = do
+  perms <- maybeIO (getPermissions path)
+  size <- maybeIO (bracket (openFile path ReadMode) hClose hFileSize)
+  modified <- maybeIO (getModificationTime path)
+  return (Info path perms size modified)
+
+traverse :: ([Info] -> [Info]) -> FilePath -> IO [Info]
+traverse order path = do
+  names <- getUsefulContent path
+  contents <- mapM getInfo (path : map (path </>) names)
+  liftM concat $ forM (order contents) $ \info -> 
+    if isDirectory info && (infoPath info /= path)
+      then ControllerVisit.traverse order (infoPath info)
+      else return [info]
+
+getUsefulContent :: FilePath -> IO [String]
+getUsefulContent path = do
+  names <- getDirectoryContents path
+  return (filter (`notElem` [".",".."]) names)
+
+isDirectory :: Info -> Bool
+isDirectory = maybe False searchable . infoPerms
+
+postOrder :: [Info] -> [Info]
+postOrder (x:xs) = if isDirectory x
+  then postOrder xs ++ [x]
+  else x:postOrder xs
+postOrder [] = []
+
